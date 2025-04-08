a584d0a530ecb9139e93d01017ddb2ab88c66e03
FeichenYu2001 <zoeyufc@gmail.com>
2025-04-08 14:21:59 -0400

    initial commit

---
+++ mongodb/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: "mongodb+srv://wanqing:Guanqh-0039@cluster0.ugop9.mongodb.net/jobportal?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
---
+++ src/Components/Data/Reviews.js
@@ -0,0 +1,53 @@
+import KevenImg from '../Images/Keven.jpg';
+import ZoeImg from '../Images/Zoe.jpg';
+import SakshiImg from '../Images/Sakshi.jpg';
+import WanqingImg from '../Images/Wanqing.jpg';
+import MichaelImg from '../Images/Michael.jpg';
+import SakshamImg from '../Images/Saksham.jpg';
+
+const ReviewsList = [
+    {
+        id: '1',
+        name: "Keven",
+        skill: "Web Designer",
+        imgsrc: KevenImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: '2',
+        name: "Zoe",
+        skill: "Data Scientist",
+        imgsrc: ZoeImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: '3',
+        name: "Sakshi",
+        skill: "Human Resources",
+        imgsrc: SakshiImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: '4',
+        name: "Qing",
+        skill: "Web Designer",
+        imgsrc: WanqingImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: '5',
+        name: "Michael",
+        skill: "Data Scientist",
+        imgsrc: MichaelImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+        id: '6',
+        name: "Verma",
+        skill: "Human Resources",
+        imgsrc: SakshamImg,
+        msg: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    }
+]
+
+export default ReviewsList;
